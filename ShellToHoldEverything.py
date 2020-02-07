ea068a19ced26a152a25543c722e441679d5c65c
MrPeaBody <James3roberts@gmail.com>
2020-02-07 16:05:44 -0700

    added comments to control flow.

--- ShellToHoldEverything.py
+++ ShellToHoldEverything.py
@@ -1,6 +1,7 @@
 ##File to contain everything for the robot.
 ## SensorAsPartOFWhole is for the motor.
-##distance1 is to get the distance1
-##distance2 is to measure the motor
-## button is to get the distance1 and send to distance2 still have to learn this step.
+##distance1 is to get the distance the motor moves
+##distance2 is to measure how far the motor should ove. 
+## button will be used as a way to mark measurements. 
+
 
