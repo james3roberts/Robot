e6c1f2c0129b3e1efc5c2c8d7a75aff7b914c3f9
James Roberts <james3roberts@gmail.com>
2020-02-07 14:58:30 -0700

    lost my mind for a bit better now

--- ShellToHoldEverything.py
+++ ShellToHoldEverything.py
@@ -2,13 +2,5 @@
 ## SensorAsPartOFWhole is for the motor.
 ##distance1 is to get the distance1
 ##distance2 is to measure the motor
-## button is to get the distance1 and send to distance2
-import SensorAsPartOFWhole.py
-import distance1.py
+## button is to get the distance1 and send to distance2 still have to learn this step.
 
-
-#going to try and get the motor to move the tape 6 inches.
-execfile('distance1.py')
-if distance in distance1 != 6:
-    execfile('SensorAsPartOFWhole.py')
-     
