f3087cf32c878b8dcd64f0812e040d26c3335655
MrPeaBody <James3roberts@gmail.com>
2020-02-07 20:57:35 -0700

    lets see how this works

--- ShellToHoldEverything.py
+++ ShellToHoldEverything.py
@@ -1,50 +1,23 @@
 ##File to contain everything for the robot.
 ## SensorAsPartOFWhole is for the motor.
 ##distance1 is to get the distance the motor moves
-##distance2 is to measure how far the motor should ove. 
-## button will be used as a way to mark measurements. 
-
-#OK i think defining a function to call distance1 and then and if statement to run the motor.
-#if that does not work then might have to wright the two together. or call them at the smae time
+##distance2 is to measure how far the motor should ove.
+## button will be used as a way to mark measurements.
 
 
-# I think i found a simpler way and there is still errors that I dont know how to fix
 
-# first try. hope this goes somewhere. 
-# information about def a function from w3school.com
+# this should get the sensor to run and stop at 6cm
+#I added the while statement to see if this will get it to keep running
+# added the print statement to get the distance. 
+# dont know what will work and cant check itto
 
-# import time
-
-# #import SensorAsPartOFWhole
-
-# #this function should get the distance from distance1 and print the value
-# def d1(mdistance):
-#     import distance1
-#     value = distance1.get_value(distance)
-#     print(value)
-
-# #this should turn on the motor.
-# def m1 (motor):
-#     import SensorAsPartOFWhole
-
-
-# if d1 != 6:
-#     m1
-# if d1 == 6:
-#     exit
-
-#the exit at the bottom should kill the program
-#This might need to be put in a loop 
 import time
 
-import distance1
 from distance1 import distance
-if distance != 6:
+while distance <=6:
+    print(distance)
     import SensorAsPartOFWhole
 else:
     exit()
 
-
-
-
-
+#if this works then we need to add another sensor and a button.
