ad141237c82e16a32923edd6fb4e9212a55230dc
MrPeaBody <James3roberts@gmail.com>
2020-02-07 17:52:29 -0700

    First try at getting them to work together

--- ShellToHoldEverything.py
+++ ShellToHoldEverything.py
@@ -8,31 +8,41 @@
 #if that does not work then might have to wright the two together. or call them at the smae time
 
 
+# I think i found a simpler way and there is still errors that I dont know how to fix
+
 # first try. hope this goes somewhere. 
 # information about def a function from w3school.com
 
+# import time
+
+# #import SensorAsPartOFWhole
+
+# #this function should get the distance from distance1 and print the value
+# def d1(mdistance):
+#     import distance1
+#     value = distance1.get_value(distance)
+#     print(value)
+
+# #this should turn on the motor.
+# def m1 (motor):
+#     import SensorAsPartOFWhole
+
+
+# if d1 != 6:
+#     m1
+# if d1 == 6:
+#     exit
+
+#the exit at the bottom should kill the program
+#This might need to be put in a loop 
 import time
 
-#import SensorAsPartOFWhole
-
-#this function should get the distance from distance1 and print the value
-def d1(mdistance):
-    import distance1
-    value = distance1.get_value(distance)
-    print(value)
-
-#this should turn on the motor.
-def m1 (motor):
+import distance1
+from distance1 import distance
+if distance != 6:
     import SensorAsPartOFWhole
-
-
-if d1 != 6:
-    m1
-if d1 == 6:
-    exit(m1)
-
-
-
+else:
+    exit()
 
 
 
