bcdd2dde63bf66d28ab7249b65337a15edcc9563
James Roberts <james3roberts@gmail.com>
2020-02-11 23:03:08 -0700

    go big or go try again

---
+++ SolidTry.py
@@ -0,0 +1,10 @@
+#maybe the proble is that I keep trying to make things to complicated
+#what if I just make 1 big program and call it a day.
+#as i start this it is 11pm and I cant sleep
+#too much stress and a feeling of needing to get a win no matter how small
+#might just be to much star wars comming out
+
+
+#the goal of this program is to combine what I already have connected to the pi
+#that is just the motor and the sensor. 
+#once this works I will add more to the pi and then add more code. 
