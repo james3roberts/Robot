e298249d81ab3f1ce5edd074a98737502c7cbba3
James Roberts <james3roberts@gmail.com>
2020-02-11 23:32:19 -0700

    combination

--- SolidTry.py
+++ SolidTry.py
@@ -42,63 +42,66 @@ while i:
         print('Distance: ', distance, "cm")
         distanceIn = (distance / 2.54)
         print('Distance: ',distanceIn, "Inches")
-        get_value = distance
-    finally:
-        GPIO.cleanup()
-if distance <10:
-    GPIO.setmode(GPIO.BCM)
-StepPins = [17,18,27,22]
+        if distance <=10:
+            GPIO.setmode(GPIO.BCM)
+            StepPins = [17,18,27,22]
 
-for pin in StepPins:
-  print ("Setup pins")
-  GPIO.setup(pin,GPIO.OUT)
-  GPIO.output(pin, False)
+            for pin in StepPins:
+                print ("Setup pins")
+                GPIO.setup(pin,GPIO.OUT)
+                GPIO.output(pin, False)
 
-Seq = [[1,0,0,1],
-       [1,0,0,0],
-       [1,1,0,0],
-       [0,1,0,0],
-       [0,1,1,0],
-       [0,0,1,0],
-       [0,0,1,1],
-       [0,0,0,1]]
+            Seq = [[1,0,0,1],
+                   [1,0,0,0],
+                   [1,1,0,0],
+                   [0,1,0,0],
+                   [0,1,1,0],
+                   [0,0,1,0],
+                   [0,0,1,1],
+                   [0,0,0,1]]
 
 
-StepCount = len(Seq)
+            StepCount = len(Seq)
 
-StepDir = 2 # Set to 1 or 2 for clockwise
+            StepDir = 2 # Set to 1 or 2 for clockwise
             # Set to -1 or -2 for anti-clockwise
 
  
-if len(sys.argv)>1:
-  WaitTime = int(sys.argv[1])/float(1000)
+            if len(sys.argv)>1:
+                WaitTime = int(sys.argv[1])/float(1000)
  
   
-else:
-  WaitTime = 10/float(1000)
+            else:
+                WaitTime = 10/float(1000)
 
-StepCounter = 0
+            StepCounter = 0
 
-while True:
+            while True:
 
-  print (StepCounter,)
-  print (Seq[StepCounter])
+                print (StepCounter,)
+                print (Seq[StepCounter])
 
-  for pin in range(0, 4):
-    xpin = StepPins[pin]
-    if Seq[StepCounter][pin]!=0:
-      print (" Enable GPIO %i" %(xpin))
-      GPIO.output(xpin, True)
-    else:
-      GPIO.output(xpin, False)
+            for pin in range(0, 4):
+                xpin = StepPins[pin]
+                if Seq[StepCounter][pin]!=0:
+                    print (" Enable GPIO %i" %(xpin))
+                    GPIO.output(xpin, True)
+                else:
+                     GPIO.output(xpin, False)
 
-  StepCounter += StepDir
+            StepCounter += StepDir
 
 
-  if (StepCounter>=StepCount):
-    StepCounter = 0
-  if (StepCounter<0):
-    StepCounter = StepCount+StepDir
+            if (StepCounter>=StepCount):
+                StepCounter = 0
+            if (StepCounter<0):
+                StepCounter = StepCount+StepDir
 
-  time.sleep(WaitTime)
-print(StepCounter)
+            time.sleep(WaitTime)
+        print(StepCounter)
+        get_value = distance
+
+
+
+    finally:
+        GPIO.cleanup()
